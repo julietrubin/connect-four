fdd460c3d089a47da6ea906e30ddc4a306025848
juliet <juliet.rubin@glorifi.com>
2023-01-06 16:54:02 -0800

    display the board with colors for player1 and player2 moves

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import * as React from "react"
 import Seo from "../components/seo"
 import "./index.scss"
+import Circle from "../components/circle"
 
 class IndexPage extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      clicked: true
+      circles: [[1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
     }
   }
 
@@ -17,9 +19,17 @@ class IndexPage extends React.Component {
 
   render() {
     return (
-      <>
-        <div className={`circle ${this.state.clicked ? "clicked" : ""}`} onClick={this.clickCircle.bind(this)}></div>
-      </>
+      <table>
+        {this.state.circles.map((row) => {
+          return <tr key={row.id}>
+            {row.map((value, index) => {
+              return <td><Circle value={value}></Circle></td>
+            })}
+          </tr>
+        })}
+        {/* {this.state.circles.map(x => x.map(y => <Circle clicked={true}></Circle>))} */}
+        {/* <div className={`circle ${this.state.clicked ? "clicked" : ""}`} onClick={this.clickCircle.bind(this)}></div> */}
+      </table>
     );
   }
 }
