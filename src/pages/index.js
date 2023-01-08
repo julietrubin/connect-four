46c9562003535d72c68dfe57d77a043cb27c7393
juliet <juliet.rubin@glorifi.com>
2023-01-08 09:34:35 -0800

    won is working and printing to console

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Seo from "../components/seo"
 import "./index.scss"
 import Circle from "../components/circle"
 
+let ROW_LENGTH = 6;
+let COLUMN_LENGTH = 7;
 class IndexPage extends React.Component {
   constructor() {
     super();
@@ -14,20 +16,79 @@ class IndexPage extends React.Component {
     }
   }
 
+  arrayContainsConnect4(array) {
+    return array.join('').includes(this.state.playerTurn == 1 ? '1111' : '2222');;
+  }
+
+  checkRow(rowIndex) {
+    return this.arrayContainsConnect4(this.state.board[rowIndex]);
+  }
+
+  checkColumn(columnIndex) {
+    return this.arrayContainsConnect4(this.state.board.map((row) => row[columnIndex]));
+  }
+
+  checkDiagonalUp(rowIndex, columnIndex) {
+    var x = columnIndex;
+    var y = rowIndex;
+    while (x > 0 && y > 0) {
+      x--;
+      y--;
+    }
+    var array = [];
+    while (x < COLUMN_LENGTH && y < ROW_LENGTH) {
+      array.push(this.state.board[y][x]);
+      x++;
+      y++;
+    }
+    return this.arrayContainsConnect4(array);
+  }
+
+  checkDiagonalDown(rowIndex, columnIndex) {
+    var x = columnIndex;
+    var y = rowIndex;
+
+    while (x > 0 && y < ROW_LENGTH - 1) {
+      y++;
+      x--;
+    }
+    var array = [];
+    while (y >= 0 && x < COLUMN_LENGTH) {
+      array.push(this.state.board[y][x]);
+      y--;
+      x++;
+    }
+    return this.arrayContainsConnect4(array);
+  }
+
+  checkIfWon(rowIndex, columnIndex) {
+    return this.checkRow(rowIndex)
+      || this.checkColumn(columnIndex)
+      || this.checkDiagonalUp(rowIndex, columnIndex)
+      || this.checkDiagonalDown(rowIndex, columnIndex);
+  }
+
   clickColumn(columnIndex) {
     var foundSpot = false;
+    var rowIndex = -1;
     // we reverse the rows because we need to check the bottom ones first
-    // then reverse again to get in correct order
+    // then reverse again to get correct order
     this.setState(state => ({
       board: state.board.reverse().map((row, index) => {
+        // TODO: separate the logic inside here
         if (!foundSpot && row[columnIndex] == 0) {
           foundSpot = true;
           row[columnIndex] = state.playerTurn;
-        }    
+          rowIndex = index;
+          if (this.checkIfWon(rowIndex, columnIndex)) {
+            console.log("won");
+          }
+        } 
         return row;
       }).reverse(),
       playerTurn: state.playerTurn == 1 ? 2 : 1
     }));
+
   }
 
   render() {
@@ -40,8 +101,6 @@ class IndexPage extends React.Component {
             })}
           </tr>
         })}
-        {/* {this.state.circles.map(x => x.map(y => <Circle clicked={true}></Circle>))} */}
-        {/* <div className={`circle ${this.state.clicked ? "clicked" : ""}`} onClick={this.clickCircle.bind(this)}></div> */}
       </tbody></table >
     );
   }
