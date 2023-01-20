dbd4af0f69cfdd96c2f0c27e3cda4660b8dcf65f
juliet <juliet.rubin@glorifi.com>
2023-01-20 11:27:37 -0800

    using hooks now and dnd is starting to work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,53 @@
-import * as React from "react"
-import Seo from "../components/seo"
 import "./index.scss"
 import Circle from "../components/circle"
 import Draggable, { DraggableCore } from 'react-draggable';
-
+import React from 'react';
+import { useState } from "react";
+import styled from "@emotion/styled";
 
 let ROW_LENGTH = 6;
 let COLUMN_LENGTH = 7;
-class IndexPage extends React.Component {
-  constructor() {
 
-    super();
+const Box = styled.div`
+    width: 200px;
+    height: 200px;
+    border: 1px solid black;
+    background: yellow;
+    cursor: move;
+    transition: ${props => props.isControlled ? `transform 0.3s` : `none`};
+  `;
 
-    this.myRef = React.createRef();
-    this.state = {
-      board: [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
-      playerTurn: 1,
-    }
+function IndexPage() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isControlled, setIsControlled] = useState(true);
+  const [board, setBoard] = useState([[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]);
+  const [playerTurn, setPlayerTurn] = useState(1);
+
+  const handleStart = () => {
+    setIsControlled(false)
+  }
+  const handleDrag = (e, data) => {
+    setPosition({ x: data.x, y: data.y });
+  };
+  const handleStop = (e, data) => {
+    setIsControlled(true)
+    setPosition({ x: data.x, y: 300 })
   }
 
-  arrayContainsConnect4(array) {
-    return array.join('').includes(this.state.playerTurn == 1 ? '1111' : '2222');;
+  const arrayContainsConnect4 = (array) => {
+    return array.join('').includes(playerTurn == 1 ? '1111' : '2222');;
   }
 
-  checkRow(rowIndex) {
-    return this.arrayContainsConnect4(this.state.board[rowIndex]);
+  const checkRow = (rowIndex) => {
+    return this.arrayContainsConnect4(board[rowIndex]);
   }
 
-  checkColumn(columnIndex) {
-    return this.arrayContainsConnect4(this.state.board.map((row) => row[columnIndex]));
+  const checkColumn = (columnIndex) => {
+    return this.arrayContainsConnect4(board.map((row) => row[columnIndex]));
   }
 
-  checkDiagonalUp(rowIndex, columnIndex) {
+  const checkDiagonalUp = (rowIndex, columnIndex) => {
     var x = columnIndex;
     var y = rowIndex;
     while (x > 0 && y > 0) {
@@ -41,14 +56,14 @@ class IndexPage extends React.Component {
     }
     var array = [];
     while (x < COLUMN_LENGTH && y < ROW_LENGTH) {
-      array.push(this.state.board[y][x]);
+      array.push(board[y][x]);
       x++;
       y++;
     }
-    return this.arrayContainsConnect4(array);
+    return arrayContainsConnect4(array);
   }
 
-  checkDiagonalDown(rowIndex, columnIndex) {
+  const checkDiagonalDown = (rowIndex, columnIndex) => {
     var x = columnIndex;
     var y = rowIndex;
 
@@ -58,85 +73,63 @@ class IndexPage extends React.Component {
     }
     var array = [];
     while (y >= 0 && x < COLUMN_LENGTH) {
-      array.push(this.state.board[y][x]);
+      array.push(board[y][x]);
       y--;
       x++;
     }
     return this.arrayContainsConnect4(array);
   }
 
-  checkIfWon(rowIndex, columnIndex) {
-    return this.checkRow(rowIndex)
-      || this.checkColumn(columnIndex)
-      || this.checkDiagonalUp(rowIndex, columnIndex)
-      || this.checkDiagonalDown(rowIndex, columnIndex);
+  const checkIfWon = (rowIndex, columnIndex) => {
+    return checkRow(rowIndex)
+      || checkColumn(columnIndex)
+      || checkDiagonalUp(rowIndex, columnIndex)
+      || checkDiagonalDown(rowIndex, columnIndex);
   }
 
-  clickColumn(columnIndex) {
+  const clickColumn = (columnIndex) => {
     var foundSpot = false;
     var rowIndex = -1;
     // we reverse the rows because we need to check the bottom ones first
     // then reverse again to get correct order
-    this.setState(state => ({
-      board: state.board.reverse().map((row, index) => {
-        // TODO: separate the logic inside here
-        if (!foundSpot && row[columnIndex] == 0) {
-          foundSpot = true;
-          row[columnIndex] = state.playerTurn;
-          rowIndex = index;
-          if (this.checkIfWon(rowIndex, columnIndex)) {
-            console.log("won");
-          }
-        } 
-        return row;
-      }).reverse(),
-      playerTurn: state.playerTurn == 1 ? 2 : 1
-    }));
-
+    // this.setState(state => ({
+    //   board: state.board.reverse().map((row, index) => {
+    //     // TODO: separate the logic inside here
+    //     if (!foundSpot && row[columnIndex] == 0) {
+    //       foundSpot = true;
+    //       row[columnIndex] = state.playerTurn;
+    //       rowIndex = index;
+    //       if (checkIfWon(rowIndex, columnIndex)) {
+    //         console.log("won");
+    //       }
+    //     }
+    //     return row;
+    //   }).reverse(),
+    //   playerTurn: state.playerTurn == 1 ? 2 : 1
+    // }));
   }
 
-  dragStart = (e, position) => {
-    // this.myRef.current = position;
-    console.log("start");
-
-    // console.log(e.target.innerHTML);
-  };
-
-  dragEnter = (e, position) => {
-    // this.myRef.current = position;
-    console.log("enter");
-    // console.log(e.target.innerHTML);
-  };
-
-  drop = (e) => {
-    console.log("drop");
-    // const copyListItems = [...list];
-    // const dragItemContent = copyListItems[dragItem.current];
-    // copyListItems.splice(dragItem.current, 1);
-    // copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-    // dragItem.current = null;
-    // dragOverItem.current = null;
-    // setList(copyListItems);
-  };
-  render() {
     return (
       <>
-        <div ref={this.myRef} className="circle player1" draggable
-          onDragStart={(e) => this.dragStart(e)}
-          onDragEnter={(e) => this.dragEnter(e)}
-          onDragEnd={this.drop}></div>
-      <table><tbody>
-        {this.state.board.map((row, rowIndex) => {
-          return <tr key={rowIndex} >
-            {row.map((value, columnIndex) => {
-              return <td key={columnIndex} onClick={() => this.clickColumn(columnIndex)}><Circle key={columnIndex} value={value}></Circle></td>
-            })}
-          </tr>
-        })}
+        < Draggable
+          position={position}
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}
+          onDragOver={() => console.log("over")}
+        >
+          <Box isControlled={isControlled}>Drag me</Box>
+        </Draggable >
+        <table><tbody>
+          {board.map((row, rowIndex) => {
+            return <tr key={rowIndex} >
+              {row.map((value, columnIndex) => {
+                return <td onDragOver={() => console.log("over")} key={columnIndex}><Circle key={columnIndex} value={value}></Circle></td>
+              })}
+            </tr>
+          })}
         </tbody></table ></>
     );
-  }
 }
-export const Head = () => <Seo />
 
-export default IndexPage
+export default IndexPage;
